7a321314d93b5155faec6441480c2f7111eb8325
--replace-all <salivon_ivan@ukr.net>
2019-08-14 23:06:16 +0300

    fixed tests

--- persistence-microservice/src/test/java/io/newage/persistence/PersistenceAppTest.java
+++ persistence-microservice/src/test/java/io/newage/persistence/PersistenceAppTest.java
@@ -1,15 +1,18 @@
 package io.newage.persistence;
 
+import io.newage.persistence.config.DataBaseConfigTest;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {PersistenceApp.class, DataBaseConfigTest.class})
+@ActiveProfiles("test")
 @DirtiesContext
 public class PersistenceAppTest {
 
